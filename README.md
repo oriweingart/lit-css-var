8753314b3c30801e26e89d537c1aef47bf19db89
ori weingart <oriweingart@users.noreply.github.com>
2021-08-17 11:53:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Webpack Frontend Starterkit
+
+[![Dependabot badge](https://flat.badgen.net/dependabot/wbkd/webpack-starter?icon=dependabot)](https://dependabot.com/)
+
+A lightweight foundation for your next webpack based frontend project.
+
+### Installation
+
+```sh
+npm install
+```
+
+### Start Dev Server
+
+```sh
+npm start
+```
+
+### Build Prod Version
+
+```sh
+npm run build
+```
+
+### Features:
+
+- ES6 Support via [babel](https://babeljs.io/) (v7)
+- JavaScript Linting via [eslint](https://eslint.org/)
+- SASS Support via [sass-loader](https://github.com/jtangelder/sass-loader)
+- Autoprefixing of browserspecific CSS rules via [postcss](https://postcss.org/) and [postcss-preset-env](https://github.com/csstools/postcss-preset-env)
+- Style Linting via [stylelint](https://stylelint.io/)
+
+When you run `npm run build` we use the [mini-css-extract-plugin](https://github.com/webpack-contrib/mini-css-extract-plugin) to move the css to a separate file. The css file gets included in the head of the `index.html`.
---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,63 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  target: 'web',
+  mode: 'development',
+  devtool: 'eval-cheap-source-map',
+  output: {
+    chunkFilename: 'js/[name].chunk.js',
+  },
+  devServer: {
+    inline: true,
+    hot: true,
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+    new ESLintPlugin({
+      extensions: 'js',
+      emitWarning: true,
+      files: Path.resolve(__dirname, '../src'),
+    }),
+    new StylelintPlugin({
+      files: Path.join('src', '**/*.s?(a|c)ss'),
+    }),
+    new ESLintPlugin({
+      emitWarning: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.js$/,
+        include: Path.resolve(__dirname, '../src'),
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+});
